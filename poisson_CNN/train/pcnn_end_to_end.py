82cc702b817b1962c5e9ab6c68944dcd437f558c
Neelappagouda <neelu065@gmail.com>
2022-03-21 19:39:43 +0530

    horovod implemented to pcnn

--- poisson_CNN/train/pcnn_end_to_end.py
+++ poisson_CNN/train/pcnn_end_to_end.py
@@ -1,56 +1,41 @@
-import argparse, json, os
-import datetime
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import json
 import tensorflow as tf
+import horovod.tensorflow.keras as hvd
+import argparse
 
-dist_strategy = tf.distribute.MultiWorkerMirroredStrategy(communication_options=tf.distribute.experimental.CommunicationOptions(
-                                        implementation=tf.distribute.experimental.CollectiveCommunication.NCCL)
-)
-# os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-# from tensorflow.python.distribute.cluster_resolver import TFConfigClusterResolver
-# cluster = tf.train.ClusterSpec({"worker": ["172.10.0.104:12345",
-#                                              "172.10.0.113:12345"]})
-#
-# # On worker 0
-# cluster_resolver = tf.distribute.cluster_resolver.SimpleClusterResolver(cluster, task_type="worker",
-#                                        task_id=0,
-#                                        num_accelerators={"GPU": 2},
-#                                        rpc_layer="grpc")
-#
-#
-# os.environ['NCCL_DEBUG'] = 'INFO'
-# os.environ.pop('TF_CONFIG', None)
-# os.environ["TF_CONFIG"] = json.dumps({
-#     'cluster': {
-#         'worker': ["172.10.0.104:12345", "172.10.0.113:12345"]
-#     },
-#     'task': {'type': 'worker', 'index': 0}
-# })
-print(os.environ.get("TF_CONFIG"))
-from tensorflow.python.eager import context
+import datetime
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+hvd.init()
+# print(">>>> hvd.rank:", hvd.rank(), "hvd.size:", hvd.size())
 
-dist_strategy.context.Context().configure_collective_ops(
-      collective_leader="'/job:worker/replica:0/task:0'")
+# Horovod: pin GPU to be used to process local rank (one GPU per process)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
-# physical_devices = tf.config.list_physical_devices('GPU')
-# print(physical_devices)
-# tf.config.experimental.set_memory_growth(physical_devices[0], True)
-# if len(physical_devices) > 0:
-#     tf.config.experimental.set_memory_growth(physical_devices[0], False)
+#if args.use_fp16:
+# tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
-from .utils import load_model_checkpoint, choose_optimizer
-from ..utils import convert_tf_object_names
-from ..models import Homogeneous_Poisson_NN_Legacy, Dirichlet_BC_NN_Legacy_2, Poisson_CNN_Legacy
-from ..losses import loss_wrapper
-from ..dataset.generators import numerical_dataset_generator, reverse_poisson_dataset_generator
+if gpus:
+    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+
+from poisson_CNN.train.utils import load_model_checkpoint, choose_optimizer
+from poisson_CNN.utils import convert_tf_object_names
+from poisson_CNN.models import Homogeneous_Poisson_NN_Legacy, Dirichlet_BC_NN_Legacy_2, Poisson_CNN_Legacy
+from poisson_CNN.losses import loss_wrapper
+from poisson_CNN.dataset.generators import numerical_dataset_generator, reverse_poisson_dataset_generator
 
 parser = argparse.ArgumentParser(description="Train the Homogeneous Poisson NN")
 parser.add_argument("config", type=str, help="Path to the configuration json for training, model and dataset parameters")
 parser.add_argument("--checkpoint_dir", type=str, help="Directory to save result checkpoints in", default=".")
 parser.add_argument("--continue_from_checkpoint", type=str, help="Continue from this checkpoint file if provided", default=None)
 parser.add_argument("--learning_rate", type=str, help="Overrides the learning rate with the provided value, or with the value from the json file if 'from_json' is the provided value", default = None)
-
 args = parser.parse_args()
+
+# Horovod: write logs on worker 0.
+verbose = 1 if hvd.rank() == 0 else 0
 
 config = convert_tf_object_names(json.load(open(args.config)))
 checkpoint_dir = args.checkpoint_dir
@@ -60,39 +45,54 @@ if 'precision' in config['training'].keys():
 
 dataset = numerical_dataset_generator(randomize_boundary_smoothness = True, exclude_zero_boundaries = False, nonzero_boundaries = ['left','right','top','bottom'], rhses = 'random', return_boundaries=True, return_dx = True, return_rhs = True, **config['dataset'])
 
-    # dist_strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.ReductionToOneDevice())#nccl causes errors on POWER9
-with dist_strategy.scope():
-    hpnn = Homogeneous_Poisson_NN_Legacy(**config['hpnn_model'])
-    dbcnn = Dirichlet_BC_NN_Legacy_2(**config['dbcnn_model'])
-    model = Poisson_CNN_Legacy(hpnn, dbcnn)
-    inp, tar = dataset.__getitem__(0)
-    out = model([x[:1] for x in inp])
-    optimizer = choose_optimizer(config['training']['optimizer'])(**config['training']['optimizer_parameters'])
-    loss = loss_wrapper(global_batch_size=config['dataset']['batch_size'], **config['training']['loss_parameters'])
-    model.compile(loss=loss,optimizer=optimizer)
+hpnn = Homogeneous_Poisson_NN_Legacy(**config['hpnn_model'])
+dbcnn = Dirichlet_BC_NN_Legacy_2(**config['dbcnn_model'])
+model = Poisson_CNN_Legacy(hpnn, dbcnn)
+inp, tar = dataset.__getitem__(0)
+out = model([x[:1] for x in inp])
 
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    cb = [
-        tf.keras.callbacks.ModelCheckpoint(checkpoint_dir + '/chkpt-epoch-{epoch:02d}.mse-{mse:.4f}',save_weights_only=True,save_best_only=True,monitor = 'loss',verbose=1),
-        tf.keras.callbacks.ReduceLROnPlateau(patience = 4,monitor='loss',min_lr=config['training']['min_learning_rate']),
-        tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
-        tf.keras.callbacks.TerminateOnNaN()
-    ]
+optimizer = choose_optimizer(config['training']['optimizer'])(**config['training']['optimizer_parameters'])
 
-    # load_model_checkpoint(model, args.continue_from_checkpoint, model_config = config['model'], sample_model_input = inp)
-    checkpoint_path = args.continue_from_checkpoint
-    if checkpoint_path is not None:
-        checkpoint_filename = tf.train.latest_checkpoint(checkpoint_path)
-        print('Attempting to load checkpoint from ' + checkpoint_filename)
-        model.load_weights(checkpoint_filename)
+# optimizer = hvd.DistributedOptimizer(optimizer)         ## step 4: binding the gradient for avg gradient calculation
+
+loss = loss_wrapper(global_batch_size=config['dataset']['batch_size'], **config['training']['loss_parameters'])
+model.compile(loss=loss,optimizer=optimizer) #, experimental_run_tf_function=False)
+
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+cb = [
+    hvd.callbacks.BroadcastGlobalVariablesCallback(0), ## step 5: Broadcasting the variables for same weights initialization on all workers or for continuing from cheakpoint.
+    hvd.callbacks.MetricAverageCallback(),
+    hvd.callbacks.LearningRateWarmupCallback(initial_lr=config['training']['optimizer_parameters']['learning_rate'], warmup_epochs=3, verbose=1),
+    tf.keras.callbacks.ReduceLROnPlateau(patience = 4,monitor='loss',min_lr=config['training']['min_learning_rate']),
+    tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
+    tf.keras.callbacks.TerminateOnNaN()
+]
+
+# Horovod: save checkpoints only on worker 0 to prevent other workers from corrupting them.
+if hvd.rank() == 0:
+    cb.append(tf.keras.callbacks.ModelCheckpoint(checkpoint_dir + '/chkpt-epoch-{epoch:02d}.mse-{mse:.4f}',save_weights_only=True,save_best_only=True,monitor = 'loss',verbose=1))
 
 
-    if args.learning_rate is not None:
-        model.optimizer.learning_rate = config['training']['optimizer_parameters']['learning_rate'] if args.learning_rate.lower() == 'from_json' else float(args.learning_rate)
+# load_model_checkpoint(model, args.continue_from_checkpoint, model_config = config['model'], sample_model_input = inp)
+checkpoint_path = args.continue_from_checkpoint
+if checkpoint_path is not None:
+    checkpoint_filename = tf.train.latest_checkpoint(checkpoint_path)
+    print('Attempting to load checkpoint from ' + checkpoint_filename)
+    model.load_weights(checkpoint_filename)
 
+
+if args.learning_rate is not None:
+    model.optimizer.learning_rate = config['training']['optimizer_parameters']['learning_rate'] if args.learning_rate.lower() == 'from_json' else float(args.learning_rate)
+
+model.optimizer.learning_rate = model.optimizer.learning_rate * hvd.size()  ## step 6: proportionate increase in learning rate.
+
+
+if hvd.size() == 0:
     model.summary()
-    model.run_eagerly = True
-    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-    model.train_on_batch(dataset,epochs=config['training']['n_epochs'],callbacks = cb)
-    # model.fit(dataset, epochs=config['training']['n_epochs'], callbacks=cb)
+model.run_eagerly = True
+
+# model.train_on_batch(dataset,epochs=config['training']['n_epochs'],callbacks = cb)
+model.fit(dataset, epochs=config['training']['n_epochs'], callbacks=cb, verbose=verbose)
+
+# print("Done")
